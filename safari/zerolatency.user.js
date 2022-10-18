b94f645cffef632a8cada9843a25f394734299a9
Lionel Martin <lionelmartin2812@gmail.com>
2022-10-18 21:11:39 +1000

    safari userscript

--- safari/zerolatency.user.js
+++ safari/zerolatency.user.js
@@ -1,10 +1,29 @@
 // ==UserScript==
-// @name        Example JS Userscript
-// @description This a standard userscript with javascript code
+// @name        Zero Latency Portal Enhancer
+// @description Zero Latency Portal Enhancer
 // @match       https://portal.zerolatencyvr.com/session/*
 // @exclude-match: *://*.*
 // @version 1.0
-// @updateURL https://www.k21p.com/example.user.js
+// @updateURL https://github.com/li0nel/zerolatency/raw/master/safari/zerolatency.user.js
 // ==/UserScript==
 
-console.log("I am an example userscript");
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  }
+  
+  function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+  }
+  
+  var mailto = 'mailto:support@zerolatency.com?bcc=' + [].slice.call(document.getElementsByClassName('email js-email')).map(function(el){
+    return el.value;
+  }).join(',')
+  
+  var emailButton = htmlToElement('<a class="button" href="' + mailto +'" id="Email" style="margin-right: 20px;">Email All</a>')
+  
+  var saveButton = document.getElementById("SaveSession");
+  insertAfter(saveButton, emailButton);
+  
