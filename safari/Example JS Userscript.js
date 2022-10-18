8be87b77c3b30fe125d0fe76ac9c31be508406a6
Lionel Martin <lionelmartin2812@gmail.com>
2022-10-18 21:04:42 +1000

    safari userscript

---
+++ safari/Example JS Userscript.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Example JS Userscript
+// @description This a standard userscript with javascript code
+// @match       https://portal.zerolatencyvr.com/session/*
+// @exclude-match: *://*.*
+// @version 1.0
+// @updateURL https://www.k21p.com/example.user.js
+// ==/UserScript==
+
+console.log("I am an example userscript");
