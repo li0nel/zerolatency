9791f7f05869f9f060777c257060bc31d63a554d
Lionel Martin <lionelmartin2812@gmail.com>
2022-10-25 20:56:40 +1000

    added subject

--- safari/zerolatency.user.js
+++ safari/zerolatency.user.js
@@ -8,22 +8,21 @@
 // ==/UserScript==
 
 function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-  }
-  
-  function htmlToElement(html) {
-    var template = document.createElement('template');
-    html = html.trim(); // Never return a text node of whitespace as the result
-    template.innerHTML = html;
-    return template.content.firstChild;
-  }
-  
-  var mailto = 'mailto:support@zerolatency.com?bcc=' + [].slice.call(document.getElementsByClassName('email js-email')).map(function(el){
-    return el.value;
-  }).join(',')
-  
-  var emailButton = htmlToElement('<a class="button" href="' + mailto +'" id="Email" style="margin-right: 20px;">Email All</a>')
-  
-  var saveButton = document.getElementById("SaveSession");
-  insertAfter(saveButton, emailButton);
-  
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function htmlToElement(html) {
+  var template = document.createElement('template');
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+var mailto = 'mailto:?subject=Your%20Zero%20Latency%20Gold%20Coast%20Photos&bcc=' + [].slice.call(document.getElementsByClassName('email js-email')).map(function(el){
+  return el.value;
+}).join(',')
+
+var emailButton = htmlToElement('<a class="button" href="' + mailto +'" id="Email" style="margin-right: 20px;">Email All</a>')
+
+var saveButton = document.getElementById("SaveSession");
+insertAfter(saveButton, emailButton);
